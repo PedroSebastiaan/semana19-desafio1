1dacdacf4ecf6ed36ae5209de500d119385900cd
PedroSebastiaan <pizarropedrov@gmail.com>
2021-10-14 18:44:49 -0300

    add: letters

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event){  
     structureWords()
+    structureLetters()
 });
 
 // FUNCTIONS
@@ -16,12 +17,16 @@ cleanText = (text) => {
 
 // Turn the text into a array
 turnInArray = (text) => {return text = text.split(' ')};
+turnInArrayToLetters = (text) => {return text = text.split('')};
 
 // Pick up the text 
 pickTheText = () => {return document.getElementById("texto-entrada").textContent};
 
-//Clean the array
+// Clean the array
 cleanTheArray = (array) => {return array.filter((element) => element !== '')};
+
+// Delete spaces
+cleanTheSpaces = (text) => {return text = text = text.replaceAll(' ', '')};
 
 // Test text
 const TESTTEXT = 'hola, esto, hola, podria, no, no, funcionar';
@@ -53,6 +58,7 @@ countDuplicates = (text) => {
     return finalArray;
 };
 
+// Print into html
 structureWords = () => {
     ary = countDuplicates(pickTheText());
     let words = document.getElementById("words");
@@ -61,3 +67,34 @@ structureWords = () => {
     };
 };
 
+// Count the letters
+countDuplicatesLetters = (text) => {
+    let data = cleanTheArray(turnInArrayToLetters(cleanTheSpaces(cleanText(text))));
+    let letters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X',
+    'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r',  's', 't', 'u', 'v', 'w', 'x',
+    'y', 'z'];
+    let finalArray = []
+    //First loop the array of duplicated words
+    for (let i = 0; i < letters.length; i++){
+        // Second loop the array of all words
+        var duplicates = 0;
+        for (let j = 0; j < data.length; j++){
+            if (letters[i] === data[j]){
+                duplicates ++;
+            }
+        };
+        finalArray.push(`<p><strong>${letters[i]}</strong>: ${duplicates}</p>`);
+    };
+    return finalArray;
+};
+
+// Print into html
+structureLetters = () => {
+    ary = countDuplicatesLetters(pickTheText());
+    let letters = document.getElementById("letters");
+    for (let i = 0; i < ary.length; i++){
+        letters.innerHTML += ary[i];
+    };
+};
